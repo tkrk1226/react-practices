05abaa61ad448ac74fafdc936eba93c861633b96
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-16 10:27:12 +0900

    kanban 220316-1027

--- kanban/src/CardList.js
+++ kanban/src/CardList.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import style from './assets/css/CardList.css'
 
-const CardList = ({title}) => {
+const CardList = ({title, cards}) => {
+
+  console.log(title, cards);
+
   return (
     <div className={style.CardList}>
         <h1>{title}</h1>
