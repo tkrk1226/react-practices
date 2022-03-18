8b63ec1626028cac054fae2cab84a16a0163299a
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-18 12:55:58 +0900

    react 220318-1255

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import './assets/scss/App.scss'
+import Emaillist from './Emaillist'
+import RegisterForm from './RegisterForm'
+import SearchBar from './SearchBar'
+import data from './assets/json/data.json'
 
 const App = () => {
   return (
-    <div>Emaillist</div>
+    <div className={'App'}>
+      <RegisterForm />
+      <SearchBar />
+      <Emaillist />
+    </div>
   )
 }
 
