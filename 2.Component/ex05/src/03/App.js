21ec9453a47c6d850b2613be1549161a5c8538b5
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-24 11:31:12 +0900

    emaillist backend&frontend 220324-1131

--- 2.Component/ex05/src/03/App.js
+++ 2.Component/ex05/src/03/App.js
@@ -5,32 +5,48 @@ import Clock from './Clock';
 export default class App extends Component {
     constructor() {
         super(...arguments);
-          
+        this.state = this.getCurrentClockTime();
     }
 
     getCurrentClockTime(){
         const date = new Date();
         const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        const session = hours >= 12 ? 'pm' : 'am';  
-        
+
         return {
-            
-        }
+            hours : ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+            minutes : ('0' + date.getMinutes()).slice(-2),
+            seconds : ('0' + date.getSeconds()).slice(-2),
+            session : hours >= 12 ? 'pm' : 'am',
+            ticks : this.state ? this.state.ticks + 1 : 0
+        };
     }
 
     render() {
         return (
             <div className='clock-display'>
                 <h2>ex05 - Component LifeCycle Practice</h2>
+                {
+                    this.state.ticks % 10 == 0 ?
+                    null : 
                     <Clock 
-                        hours={10}
-                        minutes={20}
-                        seconds={40}
-                        session={'am'}    
+                    hours={this.state.hours}
+                    minutes={this.state.minutes}
+                    seconds={this.state.seconds}
+                    session={this.state.session}    
                     />
+                }
+                    
             </div>
         );
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(() => {
+            this.setState(this.getCurrentClockTime());
+        }, 1000);
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval);
     }
 }
