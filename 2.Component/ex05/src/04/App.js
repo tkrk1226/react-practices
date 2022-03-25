f77fdf9a90b1c823e8f76c75e0d536445d74658d
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-25 09:56:30 +0900

    lifecycle 220325-0926

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -4,12 +4,44 @@ import Clock from './Clock';
 
 export default function App() {
 
+    const getCurrentClockTime = () => {
+        const date = new Date();
+        const hours = date.getHours();
+
+        return {
+            hours : ('0' + (hours == 0 ? 12 : (hours > 12 ? hours - 12 : hours))).slice(-2),
+            minutes : ('0' + date.getMinutes()).slice(-2),
+            seconds : ('0' + date.getSeconds()).slice(-2),
+            session : hours >= 12 ? 'pm' : 'am'
+        };
+    }
+
+    const [currentClocktime, setCurrentClockTime] = useState(getCurrentClockTime);
+    const [ticks, setTicks] = useState(0);
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCurrentClockTime(getCurrentClockTime()); // 여기서 blocking이 되는건 아님...?
+    //         setTicks(ticks + 1);                       
+    //     }, 1000);
+    // }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrentClockTime(getCurrentClockTime()); // 여기서 blocking이 되는건 아님...?
+            setTicks(ticks + 1);                       
+        }, 1000);
+    }, [currentClocktime]);
+   
 
     return (
+        <div>
+            <span>{ticks}</span>
             <Clock
                 message={'ex05: useEffect Hook example'}
-                hours={10} // state.hours
-                minutes={25} // state.minutes
-                seconds={40}/> // state.seconds
+                hours={currentClocktime.hours} 
+                minutes={currentClocktime.minutes} 
+                seconds={currentClocktime.seconds} /> 
+        </div>
     );
 }
