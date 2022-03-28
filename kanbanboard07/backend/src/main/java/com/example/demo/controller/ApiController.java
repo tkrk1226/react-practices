ef5b6c75dba40cf5d82e7d2dd6bd47599652d041
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-28 14:10:55 +0900

    emaillst&kananboard 220328-1410

--- kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
+++ kanbanboard07/backend/src/main/java/com/example/demo/controller/ApiController.java
@@ -6,11 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.JsonResult;
+
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 	
+	@GetMapping("/card/{}")
+	public ResponseEntity<JsonResult> readCard() {
+		return null;
+	}
 
 }
