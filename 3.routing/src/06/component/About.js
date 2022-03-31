e437c0ee1f8a5494b1b25c1c8013f0edba894eef
UijinJung <tkrk1226@ajou.ac.kr>
2022-03-31 13:14:43 +0900

    route 220331-1314

--- 3.routing/src/06/component/About.js
+++ 3.routing/src/06/component/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import SiteLayout from '../../07/layout/SiteLayout';
 import styles from '../assets/scss/component/About.scss';
-import SiteLayout from '../layout/SiteLayout';
 
 export default function About() {
     return (
         <SiteLayout>
+            <Header/>
             <div className={styles.About}>
                 <h2>kickscar 입니다.</h2>
             </div>
--- 3.routing/src/07/App.js
+++ 3.routing/src/07/App.js
@@ -4,10 +4,15 @@ import {Routes, Route} from 'react-router';
 import Main from "./component/Main";
 import Gallery from "./component/Gallery";
 import Guestbook from "./component/Guestbook";
-import About from "./component/About";
-
+import About from "./component/about/About";
+import Me from './component/about/Me';
+import Login from "./component/user/Login";
+import Settings from "./component/user/Settings";
+import Join from "./component/user/Join";
+import Error404 from "./component/error/Error404";
 
 import './assets/scss/App.scss'
+
 
 export default function App() {
     return (
@@ -16,7 +21,13 @@ export default function App() {
                 <Route path='/' element={<Main />}/>
                 <Route path='gallery' element={<Gallery />}/>
                 <Route path='guestbook' element={<Guestbook />}/>
-                <Route path='about' element={<About />}/>
+                <Route exact path='about' element={<About />}>
+                    <Route path='me' element={<Me />} />
+                </Route>
+                <Route path='user/login' element={<Login />}/>
+                <Route path='user/settings' element={<Settings />}/>
+                <Route path='user/join' element={<Join />}/>
+                <Route path='*' element={<Error404 />}/>
             </Routes>
         </Router>
     );
